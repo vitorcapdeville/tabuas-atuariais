6df0175ce7af54ae9861edfb3a629365953256e1
vitorcapdeville <vgcapdeville@hotmail.com>
2023-10-12 11:11:17 -0300

    test: inclusao de testes para alteracao de periodicidade na tabua

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,8 @@
-from unittest.mock import Mock
-
 import pytest
 from numpy import array
+
 from tabatu.multiplas_vidas import StatusVidasConjuntas, TabuaMultiplasVidas
 from tabatu.multiplos_decrementos import TabuaMDT
-
 from tabatu.unico_decremento import Tabua
 
 qx_plato = array([0.1, 0.3, 0.5, 0.7, 0.9])
@@ -12,42 +10,26 @@ qx_completo = array([0.1, 0.2, 0.4, 0.8, 1.0])
 
 
 @pytest.fixture
-def mock_tabua1(monkeypatch):
-    mock_tabua = Mock()
-    mock_tabua.tabuas = (Mock(),)
-    mock_tabua.tabuas[0].qx = Mock(return_value=array([0.1, 0.2, 0.3]))
-    mock_tabua.tabuas[0].tpx = Mock(return_value=array([0.9, 0.8, 0.7]))
-    mock_tabua.tabuas[0].tempo_futuro_maximo = Mock(return_value=3)
-    mock_tabua.periodicidade = 1
-    return mock_tabua
-
-
-@pytest.fixture
-def mock_tabua2(monkeypatch):
-    mock_tabua = Mock()
-    mock_tabua.tabuas = (Mock(),)
-    mock_tabua.tabuas[0].qx = Mock(return_value=array([0.3, 0.5, 0.8]))
-    mock_tabua.tabuas[0].tpx = Mock(return_value=array([0.7, 0.4, 0.2]))
-    mock_tabua.tabuas[0].tempo_futuro_maximo = Mock(return_value=2)
-    mock_tabua.periodicidade = 1
-    return mock_tabua
-
-@pytest.fixture
 def tabua_1dt_1():
     tabua = Tabua(array([0.1, 0.2, 0.3, 0.4, 1.0]))
     return tabua
+
 
 @pytest.fixture
 def tabua_1dt_2():
     tabua = Tabua(array([0.9, 0.8, 0.7, 0.6]))
     return tabua
 
+
 @pytest.fixture
 def tabua_mdt(tabua_1dt_1, tabua_1dt_2):
     tabua = TabuaMDT(tabua_1dt_1, causa1=tabua_1dt_2, causa_principal="causa1")
     return tabua
 
+
 @pytest.fixture
 def tabua_mvd(tabua_1dt_1, tabua_1dt_2):
-    tabua = TabuaMultiplasVidas(tabua_1dt_1, causa1=tabua_1dt_2, status=StatusVidasConjuntas.LAST)
+    tabua = TabuaMultiplasVidas(
+        tabua_1dt_1, causa1=tabua_1dt_2, status=StatusVidasConjuntas.LAST
+    )
     return tabua
--- tests/test_multiplas_vidas.py
+++ tests/test_multiplas_vidas.py
@@ -1,59 +1,118 @@
-from unittest.mock import Mock
+from unittest.mock import Mock, call
 
 import pytest
-from numpy import array, cumprod
+from numpy import cumprod
 from numpy.testing import assert_array_equal
 
-from tabatu.multiplas_vidas import TabuaMultiplasVidas, StatusVidasConjuntas
+import tabatu.multiplas_vidas as tabuas_mvd_modulo
+import tabatu.tabua_base as tabua_base_modulo
+from tabatu.multiplas_vidas import StatusVidasConjuntas, TabuaMultiplasVidas
+from tabatu.periodicidade import Periodicidade
 
 
 def test_tpx_retorna_produto_acumulado_de_um_menos_qx(tabua_1dt_1, tabua_1dt_2):
-    tabua = TabuaMultiplasVidas(tabua_1dt_1, tabua_1dt_2, status=StatusVidasConjuntas.LAST)
+    tabua = TabuaMultiplasVidas(
+        tabua_1dt_1, tabua_1dt_2, status=StatusVidasConjuntas.LAST
+    )
     assert tabua.tpx([0, 0], [0]) == 1
-    assert_array_equal(tabua.tpx([0, 0], [1, 2, 3]), cumprod(1 - tabua.qx([0, 0], [0, 1, 2])))
+    assert_array_equal(
+        tabua.tpx([0, 0], [1, 2, 3]), cumprod(1 - tabua.qx([0, 0], [0, 1, 2]))
+    )
 
 
-def test_qx_retorna_o_produto_de_qx_de_cada_tabua_quando_status_eh_last(tabua_1dt_1, tabua_1dt_2):
-    tabua = TabuaMultiplasVidas(tabua_1dt_1, tabua_1dt_2, status=StatusVidasConjuntas.LAST)
+def test_qx_retorna_o_produto_de_qx_de_cada_tabua_quando_status_eh_last(
+    tabua_1dt_1, tabua_1dt_2
+):
+    tabua = TabuaMultiplasVidas(
+        tabua_1dt_1, tabua_1dt_2, status=StatusVidasConjuntas.LAST
+    )
     resultado = tabua.qx([0, 0], [1, 2, 3])
-    esperado = tabua_1dt_1.tabuas[0].qx(0, [1, 2, 3]) * tabua_1dt_2.tabuas[0].qx(0, [1, 2, 3])
+    esperado = tabua_1dt_1.tabuas[0].qx(0, [1, 2, 3]) * tabua_1dt_2.tabuas[0].qx(
+        0, [1, 2, 3]
+    )
     assert_array_equal(resultado, esperado)
 
 
-def test_qx_retorna_um_menos_o_produto_de_1_menos_qx_quando_status_eh_joint(tabua_1dt_1, tabua_1dt_2):
-    tabua = TabuaMultiplasVidas(tabua_1dt_1, tabua_1dt_2, status=StatusVidasConjuntas.JOINT)
+def test_qx_retorna_um_menos_o_produto_de_1_menos_qx_quando_status_eh_joint(
+    tabua_1dt_1, tabua_1dt_2
+):
+    tabua = TabuaMultiplasVidas(
+        tabua_1dt_1, tabua_1dt_2, status=StatusVidasConjuntas.JOINT
+    )
     resultado = tabua.qx([0, 0], [1, 2, 3])
-    esperado = 1 - (1 - tabua_1dt_1.tabuas[0].qx(0, [1, 2, 3])) * (1 - tabua_1dt_2.tabuas[0].qx(0, [1, 2, 3]))
+    esperado = 1 - (1 - tabua_1dt_1.tabuas[0].qx(0, [1, 2, 3])) * (
+        1 - tabua_1dt_2.tabuas[0].qx(0, [1, 2, 3])
+    )
     assert_array_equal(resultado, esperado)
 
 
-def test_qx_falha_quando_tamanho_de_x_eh_incompativel_com_a_qntd_de_tabuas(tabua_1dt_1, tabua_1dt_2):
-    tabua = TabuaMultiplasVidas(tabua_1dt_1, tabua_1dt_2, status=StatusVidasConjuntas.JOINT)
+def test_qx_falha_quando_tamanho_de_x_eh_incompativel_com_a_qntd_de_tabuas(
+    tabua_1dt_1, tabua_1dt_2
+):
+    tabua = TabuaMultiplasVidas(
+        tabua_1dt_1, tabua_1dt_2, status=StatusVidasConjuntas.JOINT
+    )
     with pytest.raises(ValueError):
         tabua.qx([0], [1, 2, 3])
     with pytest.raises(ValueError):
         tabua.qx([0, 0, 0], [1, 2, 3])
 
 
-def test_tempo_futuro_max_retorna_o_menor_dos_tempos_quando_status_eh_joint(tabua_1dt_1, tabua_1dt_2):
-    tabua = TabuaMultiplasVidas(tabua_1dt_1, tabua_1dt_2, status=StatusVidasConjuntas.JOINT)
+def test_tempo_futuro_max_retorna_o_menor_dos_tempos_quando_status_eh_joint(
+    tabua_1dt_1, tabua_1dt_2
+):
+    tabua = TabuaMultiplasVidas(
+        tabua_1dt_1, tabua_1dt_2, status=StatusVidasConjuntas.JOINT
+    )
     assert tabua.tempo_futuro_maximo([0, 0]) == min(
         tabua_1dt_1.tabuas[0].tempo_futuro_maximo(0),
-        tabua_1dt_2.tabuas[0].tempo_futuro_maximo(0)
+        tabua_1dt_2.tabuas[0].tempo_futuro_maximo(0),
     )
 
 
-def test_tempo_futuro_max_retorna_o_maior_dos_tempos_quando_status_eh_last(tabua_1dt_1, tabua_1dt_2):
-    tabua = TabuaMultiplasVidas(tabua_1dt_1, tabua_1dt_2, status=StatusVidasConjuntas.LAST)
+def test_tempo_futuro_max_retorna_o_maior_dos_tempos_quando_status_eh_last(
+    tabua_1dt_1, tabua_1dt_2
+):
+    tabua = TabuaMultiplasVidas(
+        tabua_1dt_1, tabua_1dt_2, status=StatusVidasConjuntas.LAST
+    )
     assert tabua.tempo_futuro_maximo([0, 0]) == max(
         tabua_1dt_1.tabuas[0].tempo_futuro_maximo(0),
-        tabua_1dt_2.tabuas[0].tempo_futuro_maximo(0)
+        tabua_1dt_2.tabuas[0].tempo_futuro_maximo(0),
     )
 
 
-def test_tempo_futuro_max_falha_quando_o_tamanho_de_x_eh_incompativel_com_a_qntd_de_tabuas(tabua_1dt_1, tabua_1dt_2):
-    tabua = TabuaMultiplasVidas(tabua_1dt_1, tabua_1dt_2, status=StatusVidasConjuntas.LAST)
+def test_tempo_futuro_max_falha_quando_o_tamanho_de_x_eh_incompativel_com_a_qntd_de_tabuas(
+    tabua_1dt_1, tabua_1dt_2
+):
+    tabua = TabuaMultiplasVidas(
+        tabua_1dt_1, tabua_1dt_2, status=StatusVidasConjuntas.LAST
+    )
     with pytest.raises(ValueError):
         tabua.tempo_futuro_maximo([0])
     with pytest.raises(ValueError):
         tabua.tempo_futuro_maximo([0, 0, 0])
+
+
+def test_alterar_periodicidade_chama_alterar_periodicidade_da_tabua_base_e_gera_nova_tabua_mvd(
+    monkeypatch, tabua_1dt_1, tabua_1dt_2
+):
+    tabua = TabuaMultiplasVidas(
+        tabua_1dt_1, tabua_1dt_2, status=StatusVidasConjuntas.LAST
+    )
+
+    monkeypatch.setattr(
+        tabuas_mvd_modulo.Tabua, "from_tabua_base", Mock(return_value=tabua_1dt_1)
+    )
+    mock_alterar_periodicidade = Mock()
+    monkeypatch.setattr(
+        tabua_base_modulo.TabuaBase, "alterar_periodicidade", mock_alterar_periodicidade
+    )
+    nova_tabua = tabua.alterar_periodicidade(Periodicidade.MENSAL)
+
+    mock_alterar_periodicidade.assert_has_calls(
+        [call(Periodicidade.MENSAL), call(Periodicidade.MENSAL)]
+    )
+
+    assert isinstance(nova_tabua, TabuaMultiplasVidas)
+    assert nova_tabua.status == tabua.status
